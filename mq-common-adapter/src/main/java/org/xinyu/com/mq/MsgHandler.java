8274398a97a8873fa96799ba8f18aaedae34c726
2506225465@qq.com <2506225465@qq.com>
2019-07-22 01:45:32 +0800

    removed

--- mq-common-adapter/src/main/java/org/xinyu/com/mq/MsgHandler.java
+++
@@ -1,9 +0,0 @@
-package org.xinyu.com.mq;
-
-import org.xinyu.com.mq.Msg;
-
-import java.io.Serializable;
-
-public interface MsgHandler extends Serializable {
-    void processMsg(Msg msg);
-}
--- mq-common-adapter/src/main/java/org/xinyu/com/mq/ProducerHandler.java
+++
@@ -1,8 +0,0 @@
-package org.xinyu.com.mq;
-
-import java.io.Serializable;
-import java.util.Map;
-
-public interface ProducerHandler extends Serializable {
-    Map<String,Object> sendMsg(Msg msg);
-}
