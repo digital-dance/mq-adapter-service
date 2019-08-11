134673018e0d6aa62cbc8be74e83220865e99b24
2506225465@qq.com <2506225465@qq.com>
2019-08-11 16:16:47 +0800

    updated ingress

--- mq-adapter-boot-rest-provider/mq-adapter-boot-rest-provider-deploy.sh
+++ mq-adapter-boot-rest-provider/mq-adapter-boot-rest-provider-deploy.sh
@@ -9,6 +9,8 @@ kubectl delete -f mq-adapter-boot-rest-provider.yaml
 
 kubectl apply -f repository-secret.yaml
 
+docker rmi -f www.digital.dance.com:5001/repository/digital.dance.docker/mq-adapter-boot-rest-provider:v1
+
 kubectl apply -f mq-adapter-boot-rest-provider.yaml
 
 #kubectl delete -f www_mq_ingress.yaml
