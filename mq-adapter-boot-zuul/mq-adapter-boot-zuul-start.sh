7c578ffe2712e0e3200a1ab9756c2da1f2b33ce7
2506225465@qq.com <2506225465@qq.com>
2019-08-12 19:13:34 +0800

    updated ingress

--- mq-adapter-boot-zuul/mq-adapter-boot-zuul-start.sh
+++ mq-adapter-boot-zuul/mq-adapter-boot-zuul-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo '192.168.0.118 www.mq.ingress' >> /etc/hosts
+#echo '192.168.0.118 www.mq.ingress' >> /etc/hosts
 mv /jar/mq-adapter-boot-zuul.jar /app/
 java -Djava.security.egd=file:/dev/./urandom -jar /app/mq-adapter-boot-zuul.jar
