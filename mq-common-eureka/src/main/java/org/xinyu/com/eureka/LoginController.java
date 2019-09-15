a77b6506a88997695016ed3bb113cb78bfadf330
2506225465@qq.com <2506225465@qq.com>
2019-09-16 01:25:04 +0800

    updated ingress

--- mq-common-eureka/src/main/java/org/xinyu/com/eureka/LoginController.java
+++ mq-common-eureka/src/main/java/org/xinyu/com/eureka/LoginController.java
@@ -1,10 +1,16 @@
 package org.xinyu.com.eureka;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Component
 @Controller
 public class LoginController {
+
+    @Value("${spring.redis.host}")
+    public String redisHost;
 
     @RequestMapping("/eureka-server/login")
     public String loginPage() {
